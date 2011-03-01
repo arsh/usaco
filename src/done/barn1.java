387f7ac134c2a6004cf369987fead2e75a81e655
andres santana <andres.santana@gmail.com>
2011-03-01 09:54:03 -0400

    adding grade for barn1 and updated barn1.java with the one that passes the tests

--- src/done/barn1.java
+++ src/done/barn1.java
@@ -27,31 +27,31 @@ public class barn1 {
 		int C = parseInt(st.nextToken());
 
 		int[] cs = new int[C];
-		for (int i = 0; i < C; i++) 
+		for (int i = 0; i < C; i++)
 			cs[i] = parseInt(in.readLine());
-		
+		Arrays.sort(cs);
 		int[] difs = new int[C - 1];
 		int before = cs[0];
 		for (int i = 1; i < cs.length; i++) {
 			int current = cs[i];
-			difs[i - 1] = Math.abs(current - before) - 1;
+			difs[i - 1] = Math.abs(current - before - 1);
 			before = current;
 		}
 		Arrays.sort(difs);
 		int boards = countBoards(cs);
 		int total = C;
 		for (int i = 0; i < difs.length && boards > M; i++) {
-			int current = difs[i];			
-			if (current != 0) boards--;
-			if (boards <= M) break;
+			int current = difs[i];
 			total += current;
+			if (current != 0)
+				boards--;
 		}
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(probName + ".out")));
 		out.println(total);
 		out.close();
 		System.exit(0);
 	}
-	
+
 	public static int countBoards(int[] list) {
 		int boards = 0;
 		if (list.length >= 1)
