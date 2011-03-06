346d1c955b503ffc05483d239a3e8d254fc6ae67
andres santana <andres.santana@gmail.com>
2011-03-05 21:05:01 -0400

    pal method that dont care about case and non-letter characters

--- src/Test.java
+++ src/Test.java
@@ -11,32 +11,41 @@ public class Test {
 		// System.out.print(data.charAt(len - 1 - i));
 		// }
 		// System.out.println("\n****************************");
-		// System.out.println("isPal:" + isPal("MadamImAdam"));
+		System.out.println("isPal:" + isPal("Madam:I'm Adam."));
 
-		long start = System.currentTimeMillis();
-		for (int i = 0; i < 10000; i++) {
-			char c = (char) i;
-			System.out.println(c + " : " + isLetter(c));
-		}
-		System.out.println("done in " + (System.currentTimeMillis() - start) + " (ms)");
 	}
 
+	/**
+	 * Determine if an string is palindrome, case insensitive and
+	 * don't pay attention to non-letter characters
+	 */
 	public static boolean isPal(String data) {
-
 		int len = data.length();
-		for (int i = 0; i < len / 2; i++) {
-			if (data.charAt(i) != data.charAt(len - 1 - i))
+		int middle = len / 2;
+		int startCursor = 0;
+		int endCursor = len - 1;
+		while (startCursor < middle && endCursor > middle) {
+			if (!Character.isLetter(data.charAt(startCursor))) {
+				startCursor++;
+				continue;
+			}			
+			if (!Character.isLetter(data.charAt(endCursor))) {
+				endCursor--;
+				continue;
+			}
+			if (Character.toLowerCase(data.charAt(startCursor)) != Character.toLowerCase(data.charAt(endCursor)))
 				return false;
+			startCursor++;
+			endCursor--;
 		}
 		return true;
 	}
-
 	// public static char toLower(char c) {
 	// if (isLetter(c))
 	// }
 
 	public static boolean isLetter(char c) {
-		//Character.toLowerCase(ch)
+		// Character.toLowerCase(ch)
 		// determined using ASCII characters table
 		return (c > 64 && c < 91) || (c > 96 && c < 123);
 	}
